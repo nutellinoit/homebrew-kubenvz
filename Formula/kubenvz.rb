5f0b7216a2ab969ead8ccf092df8ddb3d6823628
Samuele Chiocca <samuele@nutellino.it>
2020-02-20 21:33:52 +0100

    Fixing formula?

--- Formula/kubenvz.rb
+++ Formula/kubenvz.rb
@@ -4,8 +4,8 @@ class Kubenvz < Formula
   desc "Installs Kubenvz from github"
   homepage "https://github.com/nutellinoit/kubenvz"
   url "https://github.com/nutellinoit/kubenvz/releases/download/#{KUBENVZ_VERSION}/kubenvz_osx_x64.tar.gz"
-  version #{KUBENVZ_VERSION}
-  sha256 '4a129e6d366cf983650fb97d44d5ae8395b4629dae03586207d0a253edb01ba1'
+  version "#{KUBENVZ_VERSION}"
+  sha256 "4a129e6d366cf983650fb97d44d5ae8395b4629dae03586207d0a253edb01ba1"
 
   def install
     bin.install 'kubenvz'
