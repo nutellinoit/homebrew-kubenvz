0379ffb4c81daf949184b08ad1bf4b64e0742e77
Samuele Chiocca <samuele@nutellino.it>
2020-02-20 20:56:43 +0100

    Updated version to v0.2.0

--- Formula/kubenvz.rb
+++ Formula/kubenvz.rb
@@ -1,11 +1,11 @@
 class Kubenvz < Formula
-  KUBENVZ_VERSION = "v0.1.0"
+  KUBENVZ_VERSION = "v0.2.0"
 
   desc "Installs Kubenvz from github"
   homepage "https://github.com/nutellinoit/kubenvz"
   url "https://github.com/nutellinoit/kubenvz/releases/download/#{KUBENVZ_VERSION}/kubenvz_osx_x64.tar.gz"
   version #{KUBENVZ_VERSION}
-  sha256 'd3197095403c41bf7db75dcafc33d99fcf49a1900dba3943f800869ccbcaad52'
+  sha256 '4a129e6d366cf983650fb97d44d5ae8395b4629dae03586207d0a253edb01ba1'
 
   def install
     bin.install 'kubenvz'
