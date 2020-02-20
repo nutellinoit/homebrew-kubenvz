e34d59de4b4eab93642c46743de0cf0b900b377b
Samuele Chiocca <samuele@nutellino.it>
2020-02-20 21:34:25 +0100

    hardcoding version

--- Formula/kubenvz.rb
+++ Formula/kubenvz.rb
@@ -1,10 +1,9 @@
 class Kubenvz < Formula
-  KUBENVZ_VERSION = "v0.2.0"
 
   desc "Installs Kubenvz from github"
   homepage "https://github.com/nutellinoit/kubenvz"
-  url "https://github.com/nutellinoit/kubenvz/releases/download/#{KUBENVZ_VERSION}/kubenvz_osx_x64.tar.gz"
-  version "#{KUBENVZ_VERSION}"
+  url "https://github.com/nutellinoit/kubenvz/releases/download/v0.2.0/kubenvz_osx_x64.tar.gz"
+  version "v0.2.0"
   sha256 "4a129e6d366cf983650fb97d44d5ae8395b4629dae03586207d0a253edb01ba1"
 
   def install
